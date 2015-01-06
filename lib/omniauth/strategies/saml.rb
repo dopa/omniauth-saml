54b0564b703c24d5d94175cd70baf9cd41a5f846
vcavallo <vcavallo@gmail.com>
2015-01-06 11:47:16 -0500

    Removes request phase testing

--- lib/omniauth/strategies/saml.rb
+++ lib/omniauth/strategies/saml.rb
@@ -10,7 +10,6 @@ module OmniAuth
       option :idp_sso_target_url_runtime_params, {}
 
       def request_phase
-        raise "this is happening"
         options[:assertion_consumer_service_url] ||= callback_url
         runtime_request_parameters = options.delete(:idp_sso_target_url_runtime_params)
 
